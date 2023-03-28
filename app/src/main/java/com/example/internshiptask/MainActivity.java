d27212247a5aae7b1b3b2a663ff5af02939797d5
pawan <pawansahu.code@gmail.com>
2023-03-28 15:57:17 +0530

    Added Firebase Verification using OTP

--- app/src/main/java/com/example/internshiptask/MainActivity.java
+++ app/src/main/java/com/example/internshiptask/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         Button btnFirebase = findViewById(R.id.btnFirebase);
         btnFirebase.setOnClickListener(view -> {
             Intent fb = new Intent(getApplicationContext(), FirebaseActivity.class);
